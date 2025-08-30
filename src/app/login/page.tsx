d441b711a5321f32460da6cf16b913c555572257
vedamanikanta <manikanta.vedam@gmail.com>
2025-08-30 23:43:40 +0530

    test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
+  
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
