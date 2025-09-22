0c0d49a115769e20c7c7dd7e5ff17df8a0a75e19
vedamanikanta <manikanta.vedam@gmail.com>
2025-09-22 23:50:54 +0530

    Integrating User Details component

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
+import UserDetails from "./userDetails/page";
 // import { SessionProvider } from "next-auth/react";
 
 export const metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <div>
       <div className="flex justify-end items-center" >
+        <UserDetails />
         <Image src="/profile.jpg" className="rounded-full" width={30} height={30} alt="" />
       </div>
       {children}
--- src/app/(protected)/userDetails/page.tsx
+++ src/app/(protected)/userDetails/page.tsx
@@ -1,6 +1,9 @@
 export default function UserDetails (){
     return<form>
-        <h1>helladjfalks</h1>
+        <h1>username</h1>
+        <input />
+        <h1>email</h1>
+        <input />
         <input type="submit"/>
     </form>
 
