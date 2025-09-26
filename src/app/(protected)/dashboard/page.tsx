4cd7b014ceaefb9759e019b315e610b66b857936
vedamanikanta <manikanta.vedam@gmail.com>
2025-09-26 15:29:29 +0530

    added counter

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,12 +2,14 @@
 
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext: any = createContext(null);
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
+    const [count, setCount] = useState(0);
+
   const router = useRouter();
   console.log("d;",session);
   useEffect(() => {
@@ -21,6 +23,8 @@ export default function Dashboard() {
   return (
     <UserContext.Provider value={session}>
       <div className="p-6">
+        <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
         <h1 className="text-3xl font-bold">Dashboard</h1>
         {/* <p className="mt-2">Welcome, {session?.user?.username} 🎉</p> */}
         <button
