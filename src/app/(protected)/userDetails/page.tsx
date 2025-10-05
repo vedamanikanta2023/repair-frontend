232df054a5f31d0f10f393f9075084f9125d6643
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-05 23:30:17 +0530

    added age field in user details

--- src/app/(protected)/userDetails/page.tsx
+++ src/app/(protected)/userDetails/page.tsx
@@ -4,6 +4,8 @@ export default function UserDetails (){
         <input />
         <h1>email</h1>
         <input />
+        <h1>age</h1>
+        <input type="number" />
         <input type="submit"/>
     </form>
 
