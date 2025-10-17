ededc3b297bd258891be8dc00161dbbdab2bbff9
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-18 00:19:09 +0530

    separated UI and conditional rendering for hovering the userDetials

---
+++ src/app/components/ProfileUI/ProfileUI.tsx
@@ -0,0 +1,44 @@
+"use client";
+import * as React from "react";
+import Image from "next/image";
+import { capitalizeFirstLetter } from "@/utils/utils";
+
+interface UserDetailsType {
+  id: number;
+  username: string;
+  email: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+}
+export function ProfileUI(props: { userDetails: UserDetailsType }) {
+  const [showUserDetails, setUserDetails] = React.useState(false);
+  const { userDetails } = props;
+
+  return (
+    <>
+      <div className="w-16 h-16 rounded-full overflow-hidden border-2 border-gray-300 shadow-md fixed right-2.5 top-2.5">
+        <Image
+          onMouseOver={() => setUserDetails(true)}
+          onMouseLeave={() => setUserDetails(false)}
+          src="/profile.jpg"
+          alt="User Profile Picture"
+          width={64}
+          height={64}
+          className="object-cover w-full h-full"
+        />
+      </div>
+      {showUserDetails&& userDetails && (
+        <div className="flex flex-col items-start justify-center rounded-lg p-2 gap-1 text-stone-50 bg-stone-500 fixed right-6 top-20">
+          <p className="text-sm font-medium text-stone-50  tracking-wide">
+            {capitalizeFirstLetter(userDetails.role)}
+          </p>
+          <p className="text-lg font-semibold text-stone-50">
+            {capitalizeFirstLetter(userDetails.username)}
+          </p>
+          <p className="text-sm text-stone-50">{userDetails.email}</p>
+        </div>
+      )}
+    </>
+  );
+}
