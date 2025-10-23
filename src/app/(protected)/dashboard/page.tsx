5491354bfcb4ccd0d802f3afc4f8992949ce6982
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-24 00:21:51 +0530

    removed unwanted console

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -36,8 +36,6 @@ export default function Dashboard() {
         return;
       }
 
-      console.log("Fetched user details:", data);
-      // You can return data here if needed
       return data;
     } catch (error) {
       console.error("Error fetching user details:", error);
