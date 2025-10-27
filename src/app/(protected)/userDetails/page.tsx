e161423ba1820af629ca0a413d0428c9f32db1de
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-27 23:32:07 +0530

    changed the calling the of userDetails api

--- src/app/(protected)/userDetails/page.tsx
+++ src/app/(protected)/userDetails/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { UserCard } from "@/app/components/UserCard";
+import { useGetUserDetailsQuery } from "@/services/app";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
@@ -8,44 +9,12 @@ export default function UserDetails() {
   const [userDetails, setUserDetails] = useState(null);
 
   const userId = session?.user.id || "";
-
-  const fetchUserDetails = async (id: string) => {
-    try {
-      const response = await fetch(
-        `http://10.80.221.14:5000/userdetails/${id}`,
-        { cache: "no-store" } // optional, ensures fresh data
-      );
-
-      if (!response.ok) {
-        // if API sends 404 or any non-200
-        console.warn(`User not found, status: ${response.status}`);
-        // router.push("/userDetails");
-        return;
-      }
-
-      const data = await response.json();
-
-      if (!data || Object.keys(data).length === 0) {
-        // handles case when response is empty
-        console.warn("User details are empty");
-        return;
-      }
-      setUserDetails(data);
-      return data;
-    } catch (error) {
-      console.error("Error fetching user details:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (userId) {
-      fetchUserDetails(userId);
-    }
-  }, [userId]);
-  
-  if (!userDetails) {
+  const {data, error, isLoading} = useGetUserDetailsQuery(userId,{
+    skip:!userId,
+  });
+    
+  if (isLoading&&!!!data) {
     return <h1>...loading User Details</h1>;
   }
-
-  return <UserCard user={userDetails} />;
+  return data?<UserCard user={data} />:null;
 }
--- src/app/components/UserCard/UserCard.tsx
+++ src/app/components/UserCard/UserCard.tsx
@@ -1,5 +1,5 @@
-import { useRouter } from "next/navigation";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 // TypeScript version of UserCard component using Tailwind CSS
 // Usage: <UserCard user={user} />
