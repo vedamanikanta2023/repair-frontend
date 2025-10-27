58f17b0f6e79c52edbacfd527d951f57ed960a58
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-28 01:22:22 +0530

    changed the base query

--- src/services/app.js
+++ src/services/app.js
@@ -4,10 +4,8 @@ import { getSession } from 'next-auth/react'
 export const apiSlice = createApi({
   reducerPath: 'api', // Unique reducer path for this API slice
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BASE_DOMAIN, // Replace with your actual API base URL
-    // You can add headers or other configurations here if needed
+    baseUrl: process.env.NEXT_PUBLIC_BASE_DOMAIN, // API base URL
     prepareHeaders: async(headers, { getState }) => {
-      const token = getState() // Example for adding auth token
       const session = await getSession()  // fetches active session from NextAuth
 
       if (session.accessToken) {
@@ -17,23 +15,13 @@ export const apiSlice = createApi({
     },
   }),
   endpoints: (builder) => ({
-    // Define your specific API endpoints here (queries and mutations)
-    // Example:
     getPosts: builder.query({
       query: () => '/',
     }),
     getUserDetails: builder.query({
       query: (id) => `/userdetails/${id}`,
     }),
-    // addPost: builder.mutation({
-    //   query: (newPost) => ({
-    //     url: '/posts',
-    //     method: 'POST',
-    //     body: newPost,
-    //   }),
-    // }),
   }),
 });
 
-// Export auto-generated hooks for your endpoints (if defined)
 export const { useGetPostsQuery, useGetUserDetailsQuery } = apiSlice;
