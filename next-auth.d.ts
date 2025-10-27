212a0acde66b072239c479a0553eaa544469fe3a
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-27 23:53:30 +0530

    added type to the next auth  response

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,28 +1,28 @@
 import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      username: string;
-      email?: string;
-    } & DefaultSession["user"];
-    accessToken?: string;
-  }
-
-  interface User extends DefaultUser {
+interface Session {
+  user: {
     id: string;
     username: string;
     email?: string;
-    token: string;
     accessToken:string;
-  }
+} & DefaultSession["user"];
+  accessToken?: string;
+}
+
+interface User extends DefaultUser {
+  id: string;
+  username: string;
+  email?: string;
+  token: string;
+}
 }
 
 declare module "next-auth/jwt" {
-  interface JWT {
-    id: string;
-    username: string;
-    accessToken: string;
-  }
+interface JWT {
+  id: string;
+  username: string;
+  accessToken: string;
+}
 }
