5b03d8f1b3691bff5c38a2bbd5382801c605f45c
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-08 00:33:03 +0530

    deleted unwanted code

--- src/components/Profile/Profile.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { ProfileUI } from "../ProfileUI/ProfileUI";
-import { UserDetailsType } from "@/types";
-
-
-export async function Profile() {
-      return <ProfileUI/>;
-}
