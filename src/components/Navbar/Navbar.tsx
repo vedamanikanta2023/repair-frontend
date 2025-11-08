3a318a4cdbbce92808aebeb73d4cc78b2f4108bd
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-08 23:30:39 +0530

    added  logo conditionally

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,18 +3,28 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { ProfileUI } from "../ProfileUI/ProfileUI";
+import { appLogoDark, appLogoLight } from "@/utils/constants";
+import { useState } from "react";
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const [theme,setTheme]=useState("light");
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "User Details", href: "/userdetails" },
+  ];
 
-  const links = [{ name: "Home", href: "/" },{ name: "User Details", href: "/userdetails" }];
+  const handleThemeChange = ()=>{
+    const newTheme = theme==="dark"?"light":"dark";
+    setTheme(newTheme);
+  }
 
   return (
-    <nav className="bg-gray-800 text-white shadow-md sticky top-0 z-50 w-full py-1 px-2 flex items-center justify-between">
+    <nav className="bg-yellow-900 text-white shadow-md sticky top-0 z-50 w-full py-1 px-2 flex items-center justify-between">
       <div className="flex items-center space-x-3">
         <Image
           title="Rpair"
-          src="https://res.cloudinary.com/dnlvhtiio/image/upload/v1762361217/Rpair_bcwf3g.jpg"
+          src={ theme === "dark" ? appLogoDark : appLogoLight}
           alt="Rpair Logo"
           width={120}
           height={40}
@@ -28,7 +38,7 @@ export const Navbar = () => {
               <Link
                 href={link.href}
                 className={`transition-colors duration-200 hover:text-teal-400 ${
-                  pathname === link.href ? "text-teal-400" : "text-gray-200"
+                  pathname === link.href ? "text-teal-400" : "text-white"
                 }`}
               >
                 {link.name}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,5 @@
 export const sampleProfilePic = "https://res.cloudinary.com/dnlvhtiio/image/upload/v1760979082/main-sample.png";
+
+export const appLogoLight ="https://res.cloudinary.com/dnlvhtiio/image/upload/v1762361217/Rpair_bcwf3g.jpg"
+export const appLogoDark="https://res.cloudinary.com/dnlvhtiio/image/upload/v1762543240/Gemini_Generated_Image_zauc5dzauc5dzauc_a7vxrj.png"
+          
